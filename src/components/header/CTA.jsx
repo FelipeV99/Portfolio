6d1d64f1517bf987f5df43d4b663a37d89c894ab
FelipeV99 <38900312+FelipeV99@users.noreply.github.com>
2022-06-29 20:15:23 -0500

    updated cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ import {motion} from 'framer-motion'
 const CTA = () => {
   return (
     <div className='container-CTA'>
-        <motion.a href={require("../../assets/HV Felipe Andrade.pdf")} target="_blank" className='btn btn-primary' id='btn-download-cv'
+        <motion.a href={require("../../assets/cv Felipe Andrade.pdf")} target="_blank" className='btn btn-primary' id='btn-download-cv'
         whileHover={{scale:1.03}}
         whileTap={{scale:0.97}}
         >Download CV</motion.a>
